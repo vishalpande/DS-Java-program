7b3f3d0fdaf04d1c9f47f8d4a5498856a0db9b24
vishalpande <pandevishal40@gmail.com>
2024-01-17 23:26:05 +0530

    updating the code

---
+++ Program13.java
@@ -0,0 +1,31 @@
+/**
+ * Program13
+ */
+
+ import java.util.Arrays;
+public class Program13 {
+
+    public static void main(String args[]){
+
+int arr[]=new int[5];
+
+
+Arrays.sort(arr);
+
+
+
+
+for(int i=0;i<arr.length;i++){
+
+System.out.println(arr[i]);
+
+
+}
+
+
+
+
+
+
+    }
+}
