8805489adf852cc7c0e527c0c0759c9b692e6f35
vishalpande <pandevishal40@gmail.com>
2024-01-18 10:30:59 +0530

    updating the file

--- Program13.java
+++ Program13.java
@@ -1,1 +1,24 @@
-//
+//Insertion and the deletion of the element in the array from the last end;
+
+import java.util.Scanner;
+
+public class Program13 {
+public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+
+int arr[]=new int[100];
+int N=sc.nextInt();
+
+for(int i=0;i<N;i++){
+arr[i]=sc.nextInt();
+}
+
+
+
+
+
+
+    sc.close();
+}
+    
+}
