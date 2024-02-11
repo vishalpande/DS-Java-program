d586872945dac94a73215fe19e06b399e8792e07
vishalpande <pandevishal40@gmail.com>
2024-02-11 22:53:16 +0530

    update the file

--- Program5.java
+++ Program5.java
@@ -1,33 +1,29 @@
+
 //given a value of n print only those number from 1 to N that have sum of digit =k;
 import java.util.Scanner;
+
 public class Program5 {
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
 
-        Scanner input=new Scanner(System.in);
-int N=input.nextInt();
-int K=input.nextInt();
+        Scanner input = new Scanner(System.in);
+        int N = input.nextInt();
+        int K = input.nextInt();
+        for (int i = 1; i <= N; i++) {
 
+            int sum = 0;
 
-for(int i=1;i<=N;i++){
+            for (int j = i; j > 0; j /= 10) {
 
-    int sum=0;
-    
-    for(int j=i;j>0;j/=10){
+                sum += j % 10;
 
-   sum+=j%10;
+                if (sum == K) {
+                    System.out.println(i + " : " + sum);
+                }
 
-        if(sum==K) {
-            System.out.println(i + " : " + sum);
+            }
+
         }
-
-
-    }
-
-
-}
-
-
 
     }
 }
